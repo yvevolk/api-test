7f24a5b45c703ca54d489b6f819f4df6e98e4be3
Yve Volkovaya <yvevolkovaya@gmail.com>
2024-07-05 17:10:55 +0100

    initial commit/setup

---
+++ app.ts
@@ -0,0 +1,7 @@
+function myFunc (word: string) {
+    return `hello, ${word}`;
+}
+
+console.log(myFunc('world'));
+
+module.exports = {myFunc};
---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+function myFunc(word) {
+    return `hello, ${word}`;
+}
+console.log(myFunc('world'));
+module.exports = { myFunc };
